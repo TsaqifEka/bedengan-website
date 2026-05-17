630a650f1c09c2ea56832558763623346852be63
TsaqifEka <tsaqifeka15@gmail.com>
2026-05-17 09:23:12 +0700

    fix in bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,29 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Auth;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // 1. Agar Vercel (sebagai perantara) dipercaya oleh sistem keamanan Laravel
+        $middleware->trustProxies(at: '*');
+
+        // 2. Satpam pengatur arah setelah login
+        $middleware->redirectTo(
+            guests: '/login', // Kalau belum login, arahkan ke halaman login
+            users: function () { // Kalau sudah login...
+                // Cek apakah dia punya role Admin
+                if (Auth::user() && Auth::user()->role && Auth::user()->role->name === 'Admin') {
+                    return '/dashboard'; // Lempar ke Dashboard Admin
+                }
+                return '/'; // Jika pengunjung/user biasa, lempar ke halaman utama
+            }
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
@@ -23,4 +37,4 @@ if (defined('IS_VERCEL')) {
 }
 // ------------------------------
 
-return $app; // <--
+return $app;
